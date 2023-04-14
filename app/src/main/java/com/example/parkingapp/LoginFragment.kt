568867b0b2c43de87f807dba48d9386d66fa4be6
ItsMathMage <yuliantsvetkovskiy@gmail.com>
2023-04-14 13:45:07 +0300

    Create login page and register page. Connect Firebase.

---
+++ app/src/main/java/com/example/parkingapp/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.example.parkingapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val button = view.findViewById<Button>(R.id.to_register_button)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        return view
+    }
+}
